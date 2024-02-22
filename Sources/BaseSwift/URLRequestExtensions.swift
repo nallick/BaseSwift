aa107f5edcde046b043c7adb1fda54e05fb43a29
Nick Nallick <nick@purgatorydesign.com>
2024-02-21 17:46:26 -0700

    Add URLRequest convenience extensions

--- Sources/BaseSwift/URLRequestExtensions.swift
+++ Sources/BaseSwift/URLRequestExtensions.swift
@@ -27,4 +27,86 @@ extension URLRequest {
         get { httpMethod.flatMap { HttpMethod(rawValue: $0) } }
         set { httpMethod = newValue?.rawValue }
     }
+
+    public init(url: URL, httpMethod: HttpMethod, cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy, timeoutInterval: TimeInterval = 60.0) {
+        self.init(url: url, cachePolicy: cachePolicy, timeoutInterval: timeoutInterval)
+        self.httpMethod = httpMethod.rawValue
+    }
+
+    public init(url: URL, httpMethod: HttpMethod, httpBody: Data?, contentType: String, cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy, timeoutInterval: TimeInterval = 60.0) {
+        self.init(url: url, cachePolicy: cachePolicy, timeoutInterval: timeoutInterval)
+        self.httpMethod = httpMethod.rawValue
+        self.httpBody = httpBody
+        self[httpHeader: .contentLength] = httpBody?.count
+        self[httpHeader: .contentType] = contentType
+    }
+
+    public init<Body: Encodable>(url: URL, httpMethod: HttpMethod, jsonBody: Body, cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy, timeoutInterval: TimeInterval = 60.0) throws {
+        let body = try JSONEncoder().encode(jsonBody)
+        self.init(url: url, httpMethod: httpMethod, httpBody: body, contentType: "application/json", cachePolicy: cachePolicy, timeoutInterval: timeoutInterval)
+    }
+}
+
+extension URLRequest {
+
+    public enum HttpHeaderField: Equatable, Hashable, RawRepresentable, ExpressibleByStringLiteral {
+
+        public typealias RawValue = String
+
+        public var rawValue: String {
+            if case .custom(let value) = self { return value }
+            return self.kebobCaseRawValue ?? "\(self)".capitalized
+        }
+
+        public init(rawValue: String) {
+            for builtInCase in Self.allBuiltInCases {
+                if rawValue.caseInsensitiveCompare(builtInCase.rawValue) == .orderedSame {
+                    self = builtInCase
+                    return
+                }
+            }
+
+            self = .custom(rawValue)
+        }
+
+        public init(stringLiteral value: StringLiteralType) {
+            self.init(rawValue: value)
+        }
+
+        case accept, acceptEncoding, acceptLanguage, authorization, cacheControl
+        case contentEncoding, contentLanguage, contentLength, contentLocation, contentType, date
+        case custom(String)
+
+        private var kebobCaseRawValue: String? {
+            switch self {
+                case .acceptEncoding: return "Accept-Encoding"
+                case .acceptLanguage: return "Accept-Language"
+                case .cacheControl: return "Cache-Control"
+                case .contentEncoding: return "Content-Encoding"
+                case .contentLanguage: return "Content-Language"
+                case .contentLength: return "Content-Length"
+                case .contentLocation: return "Content-Location"
+                case .contentType: return "Content-Type"
+                default: return nil
+            }
+        }
+
+        public static let allBuiltInCases: [Self] = [.accept, .acceptEncoding, .acceptLanguage, .authorization, .cacheControl, .contentEncoding, .contentLanguage, .contentLength, .contentLocation, .contentType, .date]
+    }
+
+    public subscript(httpHeader field: HttpHeaderField) -> String? {
+        get { return self.value(forHTTPHeaderField: field.rawValue) }
+        set(value) { self.setValue(value, forHTTPHeaderField: field.rawValue) }
+    }
+
+    public subscript(httpHeader field: HttpHeaderField) -> Int? {
+        get {
+            guard let stringValue = self.value(forHTTPHeaderField: field.rawValue) else { return nil }
+            return Int(stringValue)
+        }
+        set(value) {
+            let stringValue = value.map { String($0) }
+            self.setValue(stringValue, forHTTPHeaderField: field.rawValue)
+        }
+    }
 }
--- Tests/BaseSwiftTests/URLRequestExtensionTests.swift
+++ Tests/BaseSwiftTests/URLRequestExtensionTests.swift
@@ -14,19 +14,20 @@ import FoundationNetworking
 
 final class URLRequestExtensionTests: XCTestCase {
 
-    func testCaseIterableIsExhaustive() {
+    func testHttpMethodCaseIterableIsExhaustive() {
 
-    // we can't use automatic CaseIterable synthesis outside of the original module, but we can use an exhaustive switch to flag changes
+    // we can't use automatic CaseIterable synthesis outside of the original module,
+    // but we can use an exhaustive switch to flag changes at test compile time
 
-        var actualAllCased: [URLRequest.HttpMethod] = []
+        var actualAllCases: [URLRequest.HttpMethod] = []
         for method in URLRequest.HttpMethod.allCases {
             switch method {
                 case .get, .put, .post, .patch, .delete, .connect, .trace, .head:   // this list should match the list in allCases below
-                    actualAllCased.append(method)
+                    actualAllCases.append(method)
             }
         }
 
-        XCTAssertEqual(actualAllCased, URLRequest.HttpMethod.allCases)
+        XCTAssertEqual(actualAllCases, URLRequest.HttpMethod.allCases)
     }
 
     func testRawValueIsUppercasedEnum() {
@@ -35,7 +36,7 @@ final class URLRequestExtensionTests: XCTestCase {
         XCTAssertEqual(rawValues, uppercased)
     }
 
-    func testMethodSetCanBeRetreavedThroughGetAndAsHttpMethod() {
+    func testMethodSetCanBeRetrievedThroughGetAndAsHttpMethod() {
         var urlRequest = URLRequest(url: URL(string: "abc.de")!)
         for method in URLRequest.HttpMethod.allCases {
             urlRequest.method = method
@@ -44,10 +45,130 @@ final class URLRequestExtensionTests: XCTestCase {
         }
     }
 
+    func testMethodCanBeSetThroughInitializer() {
+        let expectedCachePolicy = URLRequest.CachePolicy.reloadIgnoringLocalCacheData
+        let expectedTimeout: TimeInterval = 111
+
+        for method in URLRequest.HttpMethod.allCases {
+            let urlRequest = URLRequest(url: URL(string: "abc.de")!, httpMethod: method, cachePolicy: expectedCachePolicy, timeoutInterval: expectedTimeout)
+            XCTAssertEqual(urlRequest.method, method)
+            XCTAssertEqual(urlRequest.cachePolicy, expectedCachePolicy)
+            XCTAssertEqual(urlRequest.timeoutInterval, expectedTimeout)
+        }
+    }
+
+    func testBodyAndContentTypeCanBeSetThroughInitializer() {
+        let expectedBodyData = "test data".data(using: .utf8)!
+        let expectedContentType = "test content"
+        let expectedCachePolicy = URLRequest.CachePolicy.reloadIgnoringLocalCacheData
+        let expectedTimeout: TimeInterval = 111
+
+        for method in URLRequest.HttpMethod.allCases {
+            let urlRequest = URLRequest(url: URL(string: "abc.de")!, httpMethod: method, httpBody: expectedBodyData, contentType: expectedContentType, cachePolicy: expectedCachePolicy, timeoutInterval: expectedTimeout)
+
+            XCTAssertEqual(urlRequest.method, method)
+            XCTAssertEqual(urlRequest.httpBody, expectedBodyData)
+            XCTAssertEqual(urlRequest[httpHeader: .contentLength], expectedBodyData.count)
+            XCTAssertEqual(urlRequest[httpHeader: .contentType], expectedContentType)
+            XCTAssertEqual(urlRequest.cachePolicy, expectedCachePolicy)
+            XCTAssertEqual(urlRequest.timeoutInterval, expectedTimeout)
+        }
+    }
+
+    func testJsonBodyCanBeSetThroughInitializer() throws {
+        struct JsonTestType: Encodable { let a = 1; let b = "abc" }
+        let testElement = JsonTestType()
+        let expectedBodyData = try JSONEncoder().encode(testElement)
+        let expectedCachePolicy = URLRequest.CachePolicy.reloadIgnoringLocalCacheData
+        let expectedTimeout: TimeInterval = 111
+
+        for method in URLRequest.HttpMethod.allCases {
+            let urlRequest = try URLRequest(url: URL(string: "abc.de")!, httpMethod: method, jsonBody: testElement, cachePolicy: expectedCachePolicy, timeoutInterval: expectedTimeout)
+
+            XCTAssertEqual(urlRequest.method, method)
+            XCTAssertEqual(urlRequest.httpBody, expectedBodyData)
+            XCTAssertEqual(urlRequest[httpHeader: .contentLength], expectedBodyData.count)
+            XCTAssertEqual(urlRequest[httpHeader: .contentType], "application/json")
+            XCTAssertEqual(urlRequest.cachePolicy, expectedCachePolicy)
+            XCTAssertEqual(urlRequest.timeoutInterval, expectedTimeout)
+        }
+    }
+
+    func testHttpHeaderFieldRawValues() {
+        XCTAssertEqual(URLRequest.HttpHeaderField.accept.rawValue, "Accept")
+        XCTAssertEqual(URLRequest.HttpHeaderField.authorization.rawValue, "Authorization")
+        XCTAssertEqual(URLRequest.HttpHeaderField.contentEncoding.rawValue, "Content-Encoding")
+        XCTAssertEqual(URLRequest.HttpHeaderField.contentLength.rawValue, "Content-Length")
+        XCTAssertEqual(URLRequest.HttpHeaderField.contentType.rawValue, "Content-Type")
+        XCTAssertEqual(URLRequest.HttpHeaderField.date.rawValue, "Date")
+        XCTAssertEqual(URLRequest.HttpHeaderField.custom("X-Request-ID").rawValue, "X-Request-ID")
+    }
+
+    func testHttpHeaderInitializerFindsCorrectCaseInsensitiveBuiltInEnum() {
+        for builtInField in URLRequest.HttpHeaderField.allBuiltInCases {
+            let rawString = builtInField.rawValue.uppercased()
+            let headerField = URLRequest.HttpHeaderField(rawValue: rawString)
+            XCTAssertEqual(headerField, builtInField)
+        }
+    }
+
+    func testHttpHeaderFieldFromStringLiteral() {
+        let headerField: URLRequest.HttpHeaderField = "X-Request-ID"
+        XCTAssertEqual(headerField, URLRequest.HttpHeaderField.custom("X-Request-ID"))
+    }
+
+    func testHttpHeaderFieldSubscript() {
+        var urlRequest = URLRequest(url: URL(string: "abc.de")!)
+        let testString = "test string"
+        let testInteger = 123
+
+        urlRequest[httpHeader: .accept] = testString
+        XCTAssertEqual(urlRequest[httpHeader: .accept], testString)
+        XCTAssertEqual(urlRequest[httpHeader: .accept], urlRequest.value(forHTTPHeaderField: URLRequest.HttpHeaderField.accept.rawValue))
+
+        urlRequest[httpHeader: .accept] = Optional<String>.none
+        XCTAssertEqual(urlRequest[httpHeader: .accept], Optional<String>.none)
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: URLRequest.HttpHeaderField.accept.rawValue), nil)
+
+        urlRequest[httpHeader: .contentLength] = testInteger
+        XCTAssertEqual(urlRequest[httpHeader: .contentLength], testInteger)
+        XCTAssertEqual(urlRequest[httpHeader: .contentLength], urlRequest.value(forHTTPHeaderField: URLRequest.HttpHeaderField.contentLength.rawValue))
+
+        urlRequest[httpHeader: .contentLength] = Optional<Int>.none
+        XCTAssertEqual(urlRequest[httpHeader: .contentLength], Optional<Int>.none)
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: URLRequest.HttpHeaderField.contentLength.rawValue), nil)
+    }
+
+    func testHttpHeaderFieldBuiltInCaseIterableIsExhaustive() {
+
+        // we can't use automatic CaseIterable synthesis because of the associated value for the custom case,
+        // but we can use an exhaustive switch to flag changes at test compile time
+
+        var actualAllCases: [URLRequest.HttpHeaderField] = []
+        for field in URLRequest.HttpHeaderField.allBuiltInCases {
+            switch field {
+                case .accept, .acceptEncoding, .acceptLanguage, .authorization, .cacheControl, .contentEncoding, .contentLanguage,
+                        .contentLength, .contentLocation, .contentType, .date:   // this list should match the list in allBuiltInCases below
+                    actualAllCases.append(field)
+                case .custom(_): break
+            }
+        }
+
+        XCTAssertEqual(actualAllCases, URLRequest.HttpHeaderField.allBuiltInCases)
+    }
+
     static var allTests = [
-        ("testCaseIterableIsExhaustive", testCaseIterableIsExhaustive),
+        ("testHttpMethodCaseIterableIsExhaustive", testHttpMethodCaseIterableIsExhaustive),
         ("testRawValueIsUppercasedEnum", testRawValueIsUppercasedEnum),
-        ("testMethodSetCanBeRetreavedThroughGetAndAsHttpMethod", testMethodSetCanBeRetreavedThroughGetAndAsHttpMethod),
+        ("testMethodSetCanBeRetrievedThroughGetAndAsHttpMethod", testMethodSetCanBeRetrievedThroughGetAndAsHttpMethod),
+        ("testMethodCanBeSetThroughInitializer", testMethodCanBeSetThroughInitializer),
+        ("testBodyAndContentTypeCanBeSetThroughInitializer", testBodyAndContentTypeCanBeSetThroughInitializer),
+        ("testJsonBodyCanBeSetThroughInitializer", testJsonBodyCanBeSetThroughInitializer),
+        ("testHttpHeaderFieldRawValues", testHttpHeaderFieldRawValues),
+        ("testHttpHeaderInitializerFindsCorrectCaseInsensitiveBuiltInEnum", testHttpHeaderInitializerFindsCorrectCaseInsensitiveBuiltInEnum),
+        ("testHttpHeaderFieldFromStringLiteral", testHttpHeaderFieldFromStringLiteral),
+        ("testHttpHeaderFieldSubscript", testHttpHeaderFieldSubscript),
+        ("testHttpHeaderFieldBuiltInCaseIterableIsExhaustive", testHttpHeaderFieldBuiltInCaseIterableIsExhaustive),
     ]
 }
 
